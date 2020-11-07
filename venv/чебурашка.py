f0c77d602101e96b31806647f8a62d7b7a6714f8
rodger4567 <74028469+rodger4567@users.noreply.github.com>
2020-11-08 01:19:19 +0700

    чебурашка

---
+++ venv/чебурашка.py
@@ -0,0 +1,22 @@
+import time
+
+slide1 = '0_--_0'
+slide2 = '0-0'
+
+
+time.sleep(1)
+print(slide1)
+time.sleep(1)
+print(slide2)
+time.sleep(1)
+print(slide1)
+time.sleep(1)
+print(slide2)
+time.sleep(1)
+print(slide1)
+time.sleep(1)
+print(slide2)
+time.sleep(1)
+print(slide1)
+time.sleep(1)
+print(slide2)
